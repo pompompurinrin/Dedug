805763bf52322a89b2811d1aaa6e852cd4e36c0d
pompompurinrin <153580921+pompompurinrin@users.noreply.github.com>
2023-12-27 04:08:56 +0900

    1227 musicBtn, exitBtn 구축

---
+++ Dedug_gamebook1/Assets/Script/ClickEffect.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class ClickEffect : MonoBehaviour
+{
+    public GameObject touchEffectPrefab;
+    public float effectDuration = 1.5f; // 이펙트의 지속 시간
+    public float scaleFactor = 0.5f; // 이펙트의 크기 비율
+   
+
+    void Update()
+    {
+        // 터치 입력 감지
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // 터치 시작 시
+            if (touch.phase == TouchPhase.Began)
+            {
+                // 터치한 위치로 이펙트 생성
+                CreateTouchEffect(touch.position);
+            }
+        }
+    }
+
+    void CreateTouchEffect(Vector2 position)
+    {
+        // 터치한 위치를 2D 좌표로 변환
+        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(position.x, position.y, 10f));
+
+ 
+
+        // 이펙트 생성
+        GameObject effectObject = Instantiate(touchEffectPrefab, touchPosition, Quaternion.identity);
+
+        // 크기 조절
+        effectObject.transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
+
+       
+
+        // 지정된 지속 시간 후에 이펙트를 파괴
+        Destroy(effectObject, effectDuration);
+    }
+}
+
---
+++ Dedug_gamebook1/Assets/Script/GamebookManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamebookManager : MonoBehaviour
+{
+    public Image exitPopup; // 이미지 참조 변수
+   
+    void Start()
+    {
+        // 나가기 버튼에 클릭 이벤트 등록
+        Button exitButton = transform.Find("exitBtn").GetComponent<Button>();
+        exitButton.onClick.AddListener(ShowPopup);
+
+        // 버튼에 클릭 이벤트 리스너 추가
+        bgmButton.onClick.AddListener(OnBGMButtonClicked);
+        bgmButton = transform.Find("musicBtn").GetComponent<Button>();
+        
+        
+        // 버튼의 텍스트 업데이트
+        UpdateButton();
+    }
+
+    void ShowPopup()
+    {
+        // 이미지 활성화
+        exitPopup.gameObject.SetActive(true);
+
+        // 팝업의 버튼에 클릭 이벤트 등록
+        Button closeButton = exitPopup.transform.Find("cancelBtn").GetComponent<Button>();
+        closeButton.onClick.AddListener(HidePopup);
+
+    }
+
+    void HidePopup()
+    {
+        // 이미지 비활성화
+        exitPopup.gameObject.SetActive(false);
+    }
+
+    public AudioSource bgmAudioSource; // 배경 음악을 재생하는 AudioSource
+    public Button bgmButton; // BGM On/Off를 제어하는 버튼
+    
+    public Sprite bgmOn;
+    public Sprite bgmOff;
+
+    // BGM On/Off 버튼 클릭에 대한 이벤트 핸들러
+    void OnBGMButtonClicked()
+    {
+        if (bgmAudioSource.isPlaying)
+        {
+            // BGM이 재생 중이면 정지
+            bgmAudioSource.Pause();
+            bgmButton.image.sprite = bgmOff;
+        }
+        else
+        {
+            // BGM이 정지 중이면 재생
+            bgmAudioSource.Play();
+            bgmButton.image.sprite = bgmOn;
+        }
+
+        // 버튼 텍스트 업데이트
+        UpdateButton();
+    }
+
+    // 버튼 텍스트 업데이트 메서드
+    void UpdateButton()
+    {
+        bgmButton.image.sprite = bgmAudioSource.isPlaying ? bgmOn : bgmOff;
+    }
+}
