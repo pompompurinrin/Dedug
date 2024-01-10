8c08d9d786e6bcfe889d6f63e58c0dc5a4226dd9
wjshj98 <wjshj98@naver.com>
2024-01-10 19:49:18 +0900

    굿즈csv 파일 적용

--- Dedug_game/Assets/Script/DG_Script/UiManager.cs
+++ Dedug_game/Assets/Script/DG_Script/UiManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class UiManager : MonoBehaviour
 {
@@ -10,11 +11,15 @@ public class UiManager : MonoBehaviour
     public Canvas BG_MainStory;
     public Canvas PopUpBG_MainStory;
     public Canvas PopUpBG_Goldplus;
-    public Canvas Test;   
+    public Canvas Test;
     public Canvas BG_Cha1;
     public Canvas PopUpBG_GoodsInfo;
     public Canvas BG_Cha1_Story;
-   
+
+
+    public Text GoodsNameText;
+
+
 
     public Button MainStory_Btn;
     public Button Back2Home_btn;
@@ -43,26 +48,73 @@ public class UiManager : MonoBehaviour
     public Button Cha_1_Story3_Btn;
     public Button Back2Cha_1_btn;
 
-    void Start()
-    {
-        BG_Home = GameObject.Find("BG_Home").GetComponent<Canvas>();
-        BG_MainStory = GameObject.Find("GameObjectManager").transform.Find("BG_MainStory").GetComponent<Canvas>(); 
+    /* void Start()
+     {
+         BG_Home = GameObject.Find("BG_Home").GetComponent<Canvas>();
+         BG_MainStory = GameObject.Find("GameObjectManager").transform.Find("BG_MainStory").GetComponent<Canvas>(); 
 
-        PopUpBG_MainStory = GameObject.Find("GameObjectManager").transform.Find("PopUpBG_MainStory").GetComponent<Canvas>();
-        PopUpBG_Goldplus = GameObject.Find("GameObjectManager").transform.Find("PopUpBG_Goldplus").GetComponent<Canvas>();
-        Test = GameObject.Find("GameObjectManager").transform.Find("Test").GetComponent<Canvas>();
-        BG_Cha1 = GameObject.Find("GameObjectManager").transform.Find("BG_Cha1").GetComponent<Canvas>();
-        PopUpBG_GoodsInfo = GameObject.Find("GameObjectManager").transform.Find("PopUpBG_GoodsInfo").GetComponent<Canvas>();
-        BG_Cha1_Story = GameObject.Find("GameObjectManager").transform.Find("BG_Cha1_Story").GetComponent<Canvas>();
+         PopUpBG_MainStory = GameObject.Find("PopUpBG_MainStory").GetComponent<Canvas>();
+         PopUpBG_Goldplus = GameObject.Find("GameObjectManager").transform.Find("PopUpBG_Goldplus").GetComponent<Canvas>();
+         Test = GameObject.Find("GameObjectManager").transform.Find("Test").GetComponent<Canvas>();
+         BG_Cha1 = GameObject.Find("GameObjectManager").transform.Find("BG_Cha1").GetComponent<Canvas>();
+         PopUpBG_GoodsInfo = GameObject.Find("GameObjectManager").transform.Find("PopUpBG_GoodsInfo").GetComponent<Canvas>();
+         BG_Cha1_Story = GameObject.Find("GameObjectManager").transform.Find("BG_Cha1_Story").GetComponent<Canvas>();
+
+        GoodsNameText = GameObject.Find("GameObjectManager").transform.Find("GoodsNameText").GetComponent<Text>();
 
         BG_MainStory.gameObject.SetActive(false); //GameObject.Find("GameObjectManager").transform.Find("BG_MainStory").gameObject.SetActive(true);
-        PopUpBG_MainStory.gameObject.SetActive(false);
-        PopUpBG_Goldplus.gameObject.SetActive(false);
-        Test.gameObject.SetActive(false);
-        BG_Cha1.gameObject.SetActive(false);
-        PopUpBG_GoodsInfo.gameObject.SetActive(false);
-        BG_Cha1_Story.gameObject.SetActive(false);
+         PopUpBG_MainStory.gameObject.SetActive(false);
+         PopUpBG_Goldplus.gameObject.SetActive(false);
+         Test.gameObject.SetActive(false);
+         BG_Cha1.gameObject.SetActive(false);
+         PopUpBG_GoodsInfo.gameObject.SetActive(false);
+         BG_Cha1_Story.gameObject.SetActive(false);
+     }*/
+
+    void Start()
+    {
+        FindAndAssignCanvas();
+        // 나머지 Start 내용...
+
     }
+
+    void FindAndAssignCanvas()
+    {
+        FindAndAssignCanvas("BG_Home", ref BG_Home);
+        FindAndAssignCanvas("BG_MainStory", ref BG_MainStory);
+        FindAndAssignCanvas("PopUpBG_MainStory", ref PopUpBG_MainStory);
+        FindAndAssignCanvas("PopUpBG_Goldplus", ref PopUpBG_Goldplus);
+        FindAndAssignCanvas("Test", ref Test);
+        FindAndAssignCanvas("BG_Cha1", ref BG_Cha1);
+        FindAndAssignCanvas("PopUpBG_GoodsInfo", ref PopUpBG_GoodsInfo);
+        FindAndAssignCanvas("BG_Cha1_Story", ref BG_Cha1_Story);
+    }
+
+    void FindAndAssignCanvas(string canvasName, ref Canvas canvasVariable)
+    {
+        Transform canvasTransform = GameObject.Find("GameObjectManager")?.transform.Find(canvasName);
+        if (canvasTransform != null)
+        {
+            canvasVariable = canvasTransform.GetComponent<Canvas>();
+            if (canvasVariable == null)
+            {
+                Debug.LogError($"{canvasName} Canvas component is missing.");
+            }
+        }
+        else
+        {
+            Debug.LogError($"{canvasName} Canvas not found.");
+        }
+
+        GoodsNameText = GameObject.Find("GameObjectManager").transform.Find("GoodsNameText").GetComponent<Text>();
+        if (GoodsNameText == null)
+        {
+            Debug.LogError("GoodsNameText Text component is missing.");
+        }
+    }
+
+
+
 
     public void MainStory_BtnClick()
     { 
@@ -129,11 +181,35 @@ public class UiManager : MonoBehaviour
         BG_Cha1_Story.gameObject.SetActive(false);
     }
 
-    public void OnCha_1_1011_BtnClick()
+
+    public void GoodsPopUpMessage(int targetGoodsID)
     {
+        List<Dictionary<string, object>> goodsData = CSVReader.Read("GoodsCSV");
+
+        // 굿즈 아이디가 일치하는 행 찾기
+        Dictionary<string, object> selectedGoods = goodsData.Find(goods =>
+            goods.ContainsKey("GoodsID") &&
+            goods["GoodsID"] is int && // 타입 체크를 통해 적절한 캐스팅이 가능한지 확인
+            (int)goods["GoodsID"] == targetGoodsID);
+
+        // 찾은 굿즈 정보를 PopUpBG_GoodsInfo에 반영
+        if (selectedGoods != null)
+        {
+            // 예시: GoodsName을 Text로 표시
+            Text goodsNameText = PopUpBG_GoodsInfo.transform.Find("GoodsNameText").GetComponent<Text>();
+            goodsNameText.text = selectedGoods.ContainsKey("GoodsName") ? selectedGoods["GoodsName"].ToString() : "N/A";
+        }
+
+        // PopUpBG_GoodsInfo를 활성화
         PopUpBG_GoodsInfo.gameObject.SetActive(true);
     }
 
+    public void OnCha_1_1011_BtnClick()
+    {
+        GoodsPopUpMessage(1011);
+    }
+
+   
     public void OnCha_1_1012_BtnClick()
     {
         PopUpBG_GoodsInfo.gameObject.SetActive(true);
@@ -157,6 +233,7 @@ public class UiManager : MonoBehaviour
 
     public void OnCha_1_1016_BtnClick()
     {
+       
         PopUpBG_GoodsInfo.gameObject.SetActive(true);
     }
 
