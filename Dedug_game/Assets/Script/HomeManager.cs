fdf4418c4aa71957a102950ddfd9231398a6080c
teolkongge <staysilver@naver.com>
2024-01-11 09:52:09 +0900

    0111 홈 화면 기능 구현 (100%)

--- Dedug_game/Assets/Script/HomeManager.cs
+++ Dedug_game/Assets/Script/HomeManager.cs
@@ -7,23 +7,69 @@ using UnityEngine.UI;
 public class HomeManager : MonoBehaviour
 {
     
-    public Button RequestBtn;
-    public Button RankBtn;
-    public Button CollectBtn;
-    public Button GoodsBuyBtn;
-    int NowGold;
-    int NowRank;
+   
+
+    public GameObject ChangeCharPopup;
+    public Sprite[] homeImgs;
+    public Image homeImg;
+
+
+    public Text charDialogue;
+    public Text charName;
+    int nextDia;
+
+    private int diaIndex;
+    // CSV 파일을 읽어들일 데이터 리스트
+
+    
+    // CSV 파일을 읽어들일 데이터 리스트
+    private List<Dictionary<string, object>> homeDiaSample = new List<Dictionary<string, object>>();
+    private const string homeDiaSampleFileName = "HomeDiaSample";
+    
+
+
+    
+    List<string> suaList = new List<string>();
+    List<string> badaList = new List<string>();
+    List<string> cholongList = new List<string>();
+
+    List<string> nameList = new List<string>();
 
     
 
+
     public void Start()
     {
-        NowGold = PlayerPrefs.GetInt("NowGold");
-        NowRank = PlayerPrefs.GetInt("NowRank");
-        DataManager.Instance.nowGold = NowGold;
-        DataManager.Instance.nowRank = NowRank;
+        GameObject ChangeCharPopup = GameObject.Find("ChangeCharPopup");
+        GameObject MenuUI = GameObject.Find("MenuUI");
+
+        homeDiaSample = CSVReader.Read(homeDiaSampleFileName);
         
+        
+        CharHomeList();
+        CharNameList();
+
+        charDialogue.text = suaList[nextDia];
+        nextDia++;
+        if (nextDia == suaList.Count)
+        {
+            nextDia = 0;
+        }
+        homeImg.sprite = homeImgs[0];
+        charName.text = nameList[0];
+        //시작 시 팝업을 비활성화
+
+        if (ChangeCharPopup != null)
+        {
+            if (ChangeCharPopup.activeSelf)
+            {
+                ChangeCharPopup.SetActive(false);
+            }
+
+        }  
     }
+    
+
     public void ClickRequestBtn()
     {
         SceneManager.LoadScene("RequestScene");
@@ -38,5 +84,108 @@ public class HomeManager : MonoBehaviour
         SceneManager.LoadScene("GoodsSBuycene");
     }
 
+    public void OnButtonClick_ChangeCharPopup()
+    {
+        // 캐릭터 교체 팝업을 활성화
+
+        ChangeCharPopup.SetActive(true);
+    }
+
+    public void OnButtonClick_OffChangeCharPopup()
+    {
+        // 캐릭터 교체 팝업을 비활성화
+
+        ChangeCharPopup.SetActive(false);
+    }
+
+    public void OnClickChange(int ImgNumber)
+    {
+        homeImg.sprite = homeImgs[ImgNumber];
+        diaIndex = ImgNumber;
+        nextDia = 0;
+
+        charName.text = nameList[ImgNumber];
+        OnClickCharDialogue();
+    }
+    
+    private void CharHomeList()
+    {
+            for (int i = 0; i < homeDiaSample.Count; i++)
+            {
+                if ((int)homeDiaSample[i]["Index"] == 1)
+                {
+                    suaList.Add(homeDiaSample[i]["Dialogue"].ToString());
+                }
+
+                else if ((int)homeDiaSample[i]["Index"] == 2)
+                {
+                    badaList.Add(homeDiaSample[i]["Dialogue"].ToString());
+                }
+
+                else if ((int)homeDiaSample[i]["Index"] == 3)
+                {
+                    cholongList.Add(homeDiaSample[i]["Dialogue"].ToString());
+                }
+
+            }
+     }
+
+    private void CharNameList()
+    {
+      foreach ( Dictionary<string, object > info in homeDiaSample)
+        {
+            if (!nameList.Contains(info["CharName"].ToString()))
+            {
+                nameList.Add(info["CharName"].ToString());
+            }
+            
+            
+        }
+
+    }
+    public void OnClickCharDialogue()
+    {
+
+
+        if (diaIndex == 0)
+        {
+           
+            charDialogue.text = suaList[nextDia];
+            nextDia++;
+            if (nextDia == suaList.Count)
+            {
+                nextDia = 0;
+            }
+            
+        }
+
+        else if (diaIndex == 1)
+        {
+            
+            charDialogue.text = badaList[nextDia];
+            nextDia++;
+            if (nextDia == badaList.Count)
+            {
+                nextDia = 0;
+            }
+
+        }
+
+        else if (diaIndex == 2)
+        {
+            charDialogue.text = cholongList[nextDia];
+            nextDia++;
+            if (nextDia == cholongList.Count)
+            {
+                nextDia = 0;
+            }
+
+        }
+
+
+    }
+
+
 
 }
+
--- Dedug_game/Assets/Script/TopbarManager.cs
+++ Dedug_game/Assets/Script/TopbarManager.cs
@@ -1,54 +1,71 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TopbarManager : MonoBehaviour
 {
-   
+
     int NowGold;
     int NowRank;
+    string Goods3;
+    int FeverNum;
 
     public Text GoldAmountText;
     public Text NowRankName;
-    Image NowRankImage;
-    public Button Setting;
+    public Image gradeImg;
 
     public GameObject SettingPopCanvas;
+    public GameObject MenuUI;
+    
+    
 
-    public Sprite Rank1;
-    public Sprite Rank2;
-    public Sprite Rank3;
+    Dictionary<int, Sprite> rankDic = new Dictionary<int, Sprite>();
+    public Sprite[] rankImg;
+    
 
     // CSV 파일을 읽어들일 데이터 리스트
     private List<Dictionary<string, object>> data_Dialog = new List<Dictionary<string, object>>();
     private const string RankSampleFileName = "RankSample";
     private char[] TRIM_CHARS = { ' ', '\"' };
-
-   
+    
     void Start()
     {
-     
-        
-        //데이터 매니저에서 골드값과 랭크값 찾기
-        DataManager.Instance.nowGold = PlayerPrefs.GetInt("NowGold");
-        DataManager.Instance.nowRank = PlayerPrefs.GetInt("NowRank");
+       
+        NowGold = PlayerPrefs.GetInt("NowGold");
+        NowRank = PlayerPrefs.GetInt("NowRank");
+        Goods3 = PlayerPrefs.GetString("Goods3");
+        FeverNum = PlayerPrefs.GetInt("FeverNum");
+        DataManager.Instance.nowGold = NowGold;
+        DataManager.Instance.nowRank = NowRank;
+        FeverNum = DataManager.Instance.feverNum;
 
-        GameObject SettingPopupCanvas = GameObject.Find("SettingPopupCanvas");
-        NowRankImage = GameObject.Find("GradeImg").GetComponent<Image>();
-        //시작 시 팝업을 비활성화
-        SettingPopupCanvas.SetActive(false);
-
-        // CSV 파일에서 데이터 읽기
-        data_Dialog = CSVReader.Read(RankSampleFileName);
-
+        RankImage();
         RankSetupInfo();
+       
+        GameObject SettingPopupCanvas = GameObject.Find("SettingPopupCanvas");
 
+        if (MenuUI != null)
+        {
+            if (MenuUI.activeSelf)
+            {
+                MenuUI.SetActive(false);
+            }
+
+        }
+
+        if (SettingPopupCanvas != null)
+        {
+            if (SettingPopupCanvas.activeSelf)
+            {
+                SettingPopupCanvas.SetActive(false);
+            }
+
+
+        }
+       
     }
-
     private int GetIntValue(string key)
     {
         // CSV 데이터에서 특정 키의 정수값을 가져오는 메서드
@@ -72,45 +89,69 @@ public class TopbarManager : MonoBehaviour
     void RankSetupInfo()
     {
         data_Dialog = CSVReader.Read(RankSampleFileName);
-        if (Convert.ToInt32(data_Dialog[DataManager.Instance.nowRank]["level"]) == 1)
-        {
-            NowRankImage.sprite = Rank1;
-            
-        }
-        else if (Convert.ToInt32(data_Dialog[DataManager.Instance.nowRank]["level"]) == 2)
-        {
-            NowRankImage.sprite = Rank2;
-            
-        }
-        else if (Convert.ToInt32(data_Dialog[DataManager.Instance.nowRank]["level"]) == 3)
-        {
-            NowRankImage.sprite = Rank3;
-            
-        }
+
         TopBar();
     }
 
-
-
-    // Update is called once per frame
     public void TopBar()
     {
-        if (GoldAmountText != null) 
-        { 
-        
+        if (GoldAmountText != null)
+        {
+
             GoldAmountText.text = DataManager.Instance.nowGold.ToString();
         }
-        if (NowRankName !=null)
+        if (NowRankName != null)
         {
 
             NowRankName.text = data_Dialog[DataManager.Instance.nowRank]["rank"].ToString();
         }
+
+        if (gradeImg != null)
+        {
+
+           gradeImg.sprite = rankDic[NowRank];
+        }
+
     }
+    // Start is called before the first frame update
+    
+
     public void OnButtonClick_SettingPopup()
     {
-        // 팝업을 활성화
-       
+        // 세팅 팝업을 활성화
+
         SettingPopCanvas.SetActive(true);
     }
 
+    public void OnButtonClick_OffSettingPopup()
+    {
+        //세팅 팝업을 비활성화
+
+        SettingPopCanvas.SetActive(false);
+    }
+    public void OnButtonClick_MenuUI()
+    {
+        // 메뉴 팝업을 활성화
+
+        MenuUI.SetActive(true);
+    }
+
+    public void OnButtonClick_OffMenuUI()
+    {
+        // 메뉴 팝업을 비활성화
+
+        MenuUI.SetActive(false);
+    }
+
+    public void RankImage()
+    {
+       
+        for (int i = 0; i < rankImg.Length; i++)
+        {
+            rankDic.Add(i, rankImg[i]);
+            
+        }
+
+    }
 }
+
