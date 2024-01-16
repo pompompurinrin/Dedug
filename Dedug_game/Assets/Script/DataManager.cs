93b1e499181e4a12233a41480f2cc42cfbccaa48
pompompurinrin <153580921+pompompurinrin@users.noreply.github.com>
2024-01-16 10:44:17 +0900

    0116 결과창 정말 100% (DataManager에 저장)

--- Dedug_game/Assets/Script/DataManager.cs
+++ Dedug_game/Assets/Script/DataManager.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class DataManager : Singleton<DataManager>
 {
     public List<Dictionary<string, object>> DataTable;
-    public int nowRank, nowGold, feverNum, goods1011, goods2011, goods3011, goods1012, goods2012, goods3012, goods1021, goods2021, goods3021, goods1022, goods2022, goods3022, goods1031, goods2031, goods3031, goods1032, goods2032, goods3032, activeCha_1RedDotCnt, activeCha_2RedDotCnt, activeCha_3RedDotCnt, activeC1_STORYRedDotCnt, activeC2_STORYRedDotCnt, activeC3_STORYRedDotCnt, activeMainRedDotCnt;
+    public int nowRank, nowGold, feverNum, goods1011, goods2011, goods3011, goods1012, goods2012, goods3012, goods1021, goods2021, goods3021, goods1022, goods2022, goods3022, goods1031, goods2031, goods3031, goods1032, goods2032, goods3032, goods1041, goods2041, goods3041, goods1042, goods2042, goods3042, goods1051, goods2051, goods3051, goods1052, goods2052, goods3052, goods4051, goods4052, goods4053, goods4054, goods4055, goods4056, goods4057, goods4058, goods4059, goods4060, activeCha_1RedDotCnt, activeCha_2RedDotCnt, activeCha_3RedDotCnt, activeC1_STORYRedDotCnt, activeC2_STORYRedDotCnt, activeC3_STORYRedDotCnt, activeMainRedDotCnt;
     public string goods3;
+
     //DataManager.instance.DataTable?
 
     void Awake()
@@ -47,6 +48,29 @@ public class DataManager : Singleton<DataManager>
         goods1032 = 0;
         goods2032 = 0;
         goods3032 = 0;
+        goods1041 = 0;
+        goods2041 = 0;
+        goods3041 = 0;
+        goods1042 = 0;
+        goods2042 = 0;
+        goods3042 = 0;
+        goods1041 = 0;
+        goods2041 = 0;
+        goods3041 = 0;
+        goods1052 = 0;
+        goods2052 = 0;
+        goods3052 = 0;
+        goods4051 = 0;
+        goods4052 = 0;
+        goods4053 = 0;
+        goods4054 = 0;
+        goods4055 = 0;
+        goods4056 = 0;
+        goods4057 = 0;
+        goods4058 = 0;
+        goods4059 = 0;
+        goods4060 = 0;
+
         activeCha_1RedDotCnt = 0;
         activeCha_2RedDotCnt = 0;
         activeCha_3RedDotCnt = 0;
--- Dedug_game/Assets/Script/YJMiniGameManager.cs
+++ Dedug_game/Assets/Script/YJMiniGameManager.cs
@@ -155,10 +155,10 @@ public class YJMiniGameManager : MonoBehaviour
 
 
         //PercentageTable_1에서 배열을 사용할게
-        data_Dialog = CSVReader.Read("PercentageTable_real");
+        data_Dialog = CSVReader.Read("PercentageTable");
 
 
-        UserScore = GameObject.Find("UserScoretxt").GetComponent<Text>();  //왜안되는거지ㅠㅠ
+        //UserScore = GameObject.Find("UserScoretxt").GetComponent<Text>();  //왜안되는거지ㅠㅠ
 
         // 이건 왜인지 모르겠는데 넣으라고 해서 일단 넣음
         RewardsImage.Add(Reward1);
@@ -177,7 +177,7 @@ public class YJMiniGameManager : MonoBehaviour
         beforeImg.gameObject.SetActive(true);
 
         // 게임 시간, 점수 초기화
-        gameTime = 60;
+        gameTime = 5;
         score = 0;
 
         // 게임 UI 업데이트
@@ -864,7 +864,7 @@ public class YJMiniGameManager : MonoBehaviour
             if ((int)data_Dialog[i]["Nowrank"] == rank)
             {
                 gatchPerList.Add((int)data_Dialog[i]["Percentage"]);
-                gatchIdList.Add((int)data_Dialog[i]["Percentage"]);
+                gatchIdList.Add((int)data_Dialog[i]["Goods"]);
                 string imageString = "Goods" + data_Dialog[i]["Goods"].ToString();
                 goodsSprites.Add(Resources.Load<Sprite>(imageString));
 
@@ -878,7 +878,7 @@ public class YJMiniGameManager : MonoBehaviour
 
         if (data_Dialog.Count == 0)
         {
-            data_Dialog = CSVReader.Read("PercentageTable_real");
+            data_Dialog = CSVReader.Read("PercentageTable");
         }
 
         for (int i = 0; i < RewardsImage.Count; i++)
@@ -899,23 +899,31 @@ public class YJMiniGameManager : MonoBehaviour
             RewardsImage[i].sprite = rewardGoods[i];
             RewardsImage[i].gameObject.SetActive(true); // 바뀌었으니 켜라
         }
+
+        Save();
     }
 
-    void GetItems(int maxValue) // 뽑기. 랜덤 최대값 받기
+    void GetItems(int maxValue)
     {
-        int randValue = UnityEngine.Random.Range(0, maxValue); // 뽑을 애의 해당 가중치량
-        int checkUpper = 0; // 가중치 체크용
+        int randValue = UnityEngine.Random.Range(0, maxValue);
+        Debug.Log("Random Value: " + randValue);
+
+        int checkUpper = 0;
         int checkLower = 0;
+
         for (int i = 0; i < gatchIdList.Count; i++)
         {
             checkUpper += gatchPerList[i];
+            Debug.Log("Check Upper: " + checkUpper);
+
             if (i == 0)
             {
-                checkUpper += gatchPerList[i];
                 if (randValue < checkUpper)
                 {
                     rewards.Add(gatchIdList[i]);
                     rewardGoods.Add(goodsSprites[i]);
+                    Debug.Log("Reward Added: " + gatchIdList[i]);
+                    break;
                 }
             }
             else
@@ -924,10 +932,12 @@ public class YJMiniGameManager : MonoBehaviour
                 {
                     rewards.Add(gatchIdList[i]);
                     rewardGoods.Add(goodsSprites[i]);
+                    Debug.Log("Reward Added: " + gatchIdList[i]);
+                    break;
                 }
             }
-            checkLower = checkUpper;
 
+            checkLower = checkUpper;
         }
     }
 
@@ -956,19 +966,149 @@ public class YJMiniGameManager : MonoBehaviour
 
         GetGoods(_count);
 
-        Save();
+        
     }
 
 
     public void Save()
     {
-        // PlayerPrefs에 현재 값 저장
+        for (int i = 0; i < rewards.Count; i++)
+        {
+            int rewardId = rewards[i];
+
+            // 보상을 ID에 따라 DataManager.Instance에 매핑
+            switch (rewardId)
+            {
+                case 1011:
+                    DataManager.Instance.goods1011++;
+                    break;
+                case 1012:
+                    DataManager.Instance.goods1012++;
+                    break;
+                case 1021:
+                    DataManager.Instance.goods1021++;
+                    break;
+                case 1022:
+                    DataManager.Instance.goods1022++;
+                    break;
+                case 1031:
+                    DataManager.Instance.goods1031++;
+                    break;
+                case 1032:
+                    DataManager.Instance.goods1032++;
+                    break;
+                case 1041:
+                    DataManager.Instance.goods1041++;
+                    break;
+                case 1042:
+                    DataManager.Instance.goods1042++;
+                    break;
+                case 1051:
+                    DataManager.Instance.goods1051++;
+                    break;
+                case 1052:
+                    DataManager.Instance.goods1052++;
+                    break;
+                case 2011:
+                    DataManager.Instance.goods2011++;
+                    break;
+                case 2012:
+                    DataManager.Instance.goods2012++;
+                    break;
+                case 2022:
+                    DataManager.Instance.goods2022++;
+                    break;
+                case 2031:
+                    DataManager.Instance.goods2031++;
+                    break;
+                case 2041:
+                    DataManager.Instance.goods2041++;
+                    break;
+                case 2042:
+                    DataManager.Instance.goods2042++;
+                    break;
+                case 2051:
+                    DataManager.Instance.goods2051++;
+                    break;
+                case 2052:
+                    DataManager.Instance.goods2052++;
+                    break;
+                case 3011:
+                    DataManager.Instance.goods3011++;
+                    break;
+                case 3012:
+                    DataManager.Instance.goods3012++;
+                    break;
+                case 3021:
+                    DataManager.Instance.goods3021++;
+                    break;
+                case 3022:
+                    DataManager.Instance.goods3022++;
+                    break;
+                case 3031:
+                    DataManager.Instance.goods3031++;
+                    break;
+                case 3032:
+                    DataManager.Instance.goods3032++;
+                    break;
+                case 3041:
+                    DataManager.Instance.goods3041++;
+                    break;
+                case 3042:
+                    DataManager.Instance.goods3042++;
+                    break;
+                case 3051:
+                    DataManager.Instance.goods3051++;
+                    break;
+                case 4051:
+                    DataManager.Instance.goods4051++;
+                    break;
+                case 4052:
+                    DataManager.Instance.goods4052++;
+                    break;
+                case 4053:
+                    DataManager.Instance.goods4053++;
+                    break;
+                case 4054:
+                    DataManager.Instance.goods4054++;
+                    break;
+                case 4055:
+                    DataManager.Instance.goods4055++;
+                    break;
+                case 4056:
+                    DataManager.Instance.goods4056++;
+                    break;
+                case 4057:
+                    DataManager.Instance.goods4057++;
+                    break;
+                case 4058:
+                    DataManager.Instance.goods4058++;
+                    break;
+                case 4059:
+                    DataManager.Instance.goods4059++;
+                    break;
+                case 4060:
+                    DataManager.Instance.goods4060++;
+                    break;
+
+                default:
+                    break;
+
+                // 다른 보상 ID에 대한 케이스 추가...
+            }
+
+        }
+            // PlayerPrefs에 현재 값 저장
         PlayerPrefs.SetInt("Goods1011", DataManager.Instance.goods1011);
         PlayerPrefs.SetInt("Goods1012", DataManager.Instance.goods1012);
         PlayerPrefs.SetInt("Goods1021", DataManager.Instance.goods1021);
         PlayerPrefs.SetInt("Goods1022", DataManager.Instance.goods1022);
         PlayerPrefs.SetInt("Goods1031", DataManager.Instance.goods1031);
         PlayerPrefs.SetInt("Goods1032", DataManager.Instance.goods1032);
+        PlayerPrefs.SetInt("Goods1041", DataManager.Instance.goods1041);
+        PlayerPrefs.SetInt("Goods1042", DataManager.Instance.goods1042);
+        PlayerPrefs.SetInt("Goods1051", DataManager.Instance.goods1051);
+        PlayerPrefs.SetInt("Goods1052", DataManager.Instance.goods1052);
 
         PlayerPrefs.SetInt("Goods2011", DataManager.Instance.goods2011);
         PlayerPrefs.SetInt("Goods2012", DataManager.Instance.goods2012);
@@ -976,6 +1116,10 @@ public class YJMiniGameManager : MonoBehaviour
         PlayerPrefs.SetInt("Goods2022", DataManager.Instance.goods2022);
         PlayerPrefs.SetInt("Goods2031", DataManager.Instance.goods2031);
         PlayerPrefs.SetInt("Goods2032", DataManager.Instance.goods2032);
+        PlayerPrefs.SetInt("Goods2041", DataManager.Instance.goods2041);
+        PlayerPrefs.SetInt("Goods2042", DataManager.Instance.goods2042);
+        PlayerPrefs.SetInt("Goods2051", DataManager.Instance.goods2051);
+        PlayerPrefs.SetInt("Goods2052", DataManager.Instance.goods2052);
 
         PlayerPrefs.SetInt("Goods3011", DataManager.Instance.goods3011);
         PlayerPrefs.SetInt("Goods3012", DataManager.Instance.goods3012);
@@ -983,14 +1127,72 @@ public class YJMiniGameManager : MonoBehaviour
         PlayerPrefs.SetInt("Goods3022", DataManager.Instance.goods3022);
         PlayerPrefs.SetInt("Goods3031", DataManager.Instance.goods3031);
         PlayerPrefs.SetInt("Goods3032", DataManager.Instance.goods3032);
+        PlayerPrefs.SetInt("Goods3041", DataManager.Instance.goods3041);
+        PlayerPrefs.SetInt("Goods3042", DataManager.Instance.goods3042);
+        PlayerPrefs.SetInt("Goods3051", DataManager.Instance.goods3051);
+        PlayerPrefs.SetInt("Goods3052", DataManager.Instance.goods3052);
+
+        PlayerPrefs.SetInt("Goods4051", DataManager.Instance.goods4051);
+        PlayerPrefs.SetInt("Goods4052", DataManager.Instance.goods4052);
+        PlayerPrefs.SetInt("Goods4053", DataManager.Instance.goods4053);
+        PlayerPrefs.SetInt("Goods4054", DataManager.Instance.goods4054);
+        PlayerPrefs.SetInt("Goods4055", DataManager.Instance.goods4055);
+        PlayerPrefs.SetInt("Goods4056", DataManager.Instance.goods4056);
+        PlayerPrefs.SetInt("Goods4057", DataManager.Instance.goods4057);
+        PlayerPrefs.SetInt("Goods4058", DataManager.Instance.goods4058);
+        PlayerPrefs.SetInt("Goods4059", DataManager.Instance.goods4059);
+        PlayerPrefs.SetInt("Goods4060", DataManager.Instance.goods4060);
+
         PlayerPrefs.Save();
+
+        Debug.Log(DataManager.Instance.goods1011);
+        Debug.Log(DataManager.Instance.goods1012);
+        Debug.Log(DataManager.Instance.goods2011);
+        Debug.Log(DataManager.Instance.goods2012);
+        Debug.Log(DataManager.Instance.goods3011);
+        Debug.Log(DataManager.Instance.goods3012);
+        Debug.Log(DataManager.Instance.goods1021);
+        Debug.Log(DataManager.Instance.goods1022);
+        Debug.Log(DataManager.Instance.goods2021);
+        Debug.Log(DataManager.Instance.goods2022);
+        Debug.Log(DataManager.Instance.goods3021);
+        Debug.Log(DataManager.Instance.goods3022);
+        Debug.Log(DataManager.Instance.goods1031);
+        Debug.Log(DataManager.Instance.goods1032);
+        Debug.Log(DataManager.Instance.goods2031);
+        Debug.Log(DataManager.Instance.goods2032);
+        Debug.Log(DataManager.Instance.goods3031);
+        Debug.Log(DataManager.Instance.goods3032);
+        Debug.Log(DataManager.Instance.goods1041);
+        Debug.Log(DataManager.Instance.goods1042);
+        Debug.Log(DataManager.Instance.goods2041);
+        Debug.Log(DataManager.Instance.goods2042);
+        Debug.Log(DataManager.Instance.goods3041);
+        Debug.Log(DataManager.Instance.goods3042);
+        Debug.Log(DataManager.Instance.goods1051);
+        Debug.Log(DataManager.Instance.goods1052);
+        Debug.Log(DataManager.Instance.goods2051);
+        Debug.Log(DataManager.Instance.goods2052);
+        Debug.Log(DataManager.Instance.goods3051);
+        Debug.Log(DataManager.Instance.goods3052);
+        Debug.Log(DataManager.Instance.goods4051);
+        Debug.Log(DataManager.Instance.goods4052);
+        Debug.Log(DataManager.Instance.goods4053);
+        Debug.Log(DataManager.Instance.goods4054);
+        Debug.Log(DataManager.Instance.goods4055);
+        Debug.Log(DataManager.Instance.goods4056);
+        Debug.Log(DataManager.Instance.goods4057);
+        Debug.Log(DataManager.Instance.goods4058);
+        Debug.Log(DataManager.Instance.goods4059);
+        Debug.Log(DataManager.Instance.goods4060);
+
+
 
     }
 
     public void RestartClick()
     {
         Start();
-
     }
 
     public void HomeClick()
