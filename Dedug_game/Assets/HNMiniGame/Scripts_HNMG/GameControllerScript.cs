5a41b55affd60e52a9cf42de28ea26dd0f88cd53
H\a3715 <a37151628@gmail.com>
2024-01-16 23:40:38 +0900

    0116 결과창 관련 스크립트 수정

--- Dedug_game/Assets/HNMiniGame/Scripts_HNMG/GameControllerScript.cs
+++ Dedug_game/Assets/HNMiniGame/Scripts_HNMG/GameControllerScript.cs
@@ -22,31 +22,17 @@ public class GameControllerScript : MonoBehaviour
     // 이미지 사이의 간격 설정
     public const float Xspace = 260f;
     public const float Yspace = -300f;
+   
+    
+    // 스코어 초기값
+    public int score = 0;
+
+    // 메인 BGM
+    public AudioSource Main_BGM2;
 
     // 게임 시작 이미지 및 사용될 스프라이트 배열
-    [SerializeField] private MainImageScript startObject;
-    [SerializeField] private Sprite[] images;
-
-    // 위치를 무작위로 섞는 함수
-    private int[] Randomiser(int[] locations)
-    {
-        int[] array = locations.Clone() as int[];
-        for (int i = 0; i < array.Length; i++)
-        {
-            int newArray = array[i];
-            int j = Random.Range(i, array.Length);
-            array[i] = array[j];
-            array[j] = newArray;
-        }
-        return array;
-    }
-
-
-    public int score = 0;
-   // 스코어 초기값
-
-
-    public AudioSource Main_BGM2;
+    public MainImageScript startObject;
+    public Sprite[] images;
 
     // UI 요소들
     public Image ResultBG;
@@ -66,23 +52,35 @@ public class GameControllerScript : MonoBehaviour
     public Image Reward2;
     public Image Reward3;
 
-    int test;
-
     // 리워드 리스트 선언을 여기서 시킴
     public List<Image> RewardsImage = new List<Image>();
-
     public List<Dictionary<string, object>> data_Dialog = new List<Dictionary<string, object>>();
 
+
+
+
+    // 위치를 무작위로 섞는 함수
+    private int[] Randomiser(int[] locations)
+    {
+        int[] array = locations.Clone() as int[];
+        for (int i = 0; i < array.Length; i++)
+        {
+            int newArray = array[i];
+            int j = Random.Range(i, array.Length);
+            array[i] = array[j];
+            array[j] = newArray;
+        }
+        return array;
+    }
 
     
     private void Start()
     {
-        // 게임 시작 시 호출되는 함수
-        StartGame();
+        StartGame(); // 게임 시작
 
-        Main_BGM2.Play();  // 재생
-        correct_sfx.Stop();
-        error_sfx.Stop();
+        Main_BGM2.Play();    // 메인 BGM 재생
+        correct_sfx.Stop();  // correct_sfx 정지
+        error_sfx.Stop();    // correct_sfx 정지
 
         //PercentageTable_1에서 배열을 사용할게
         data_Dialog = CSVReader.Read("PercentageTable_real");
@@ -95,6 +93,8 @@ public class GameControllerScript : MonoBehaviour
         Setting();
 
     }
+
+
     // 게임이 시작될 때 호출되는 함수
     private void StartGame()
     {
@@ -139,14 +139,11 @@ public class GameControllerScript : MonoBehaviour
                     gameImage.transform.position = new Vector3(positionX, positionY, startPosition.z);
                     gameImage.transform.SetParent(parent, false);
 
-
                 }
-
-
-
             }
         }
     }
+
 
     // 열려진 이미지를 추적하고, 맞는지 확인하는 함수
     private MainImageScript firstOpen;
@@ -196,12 +193,11 @@ public class GameControllerScript : MonoBehaviour
         if (firstOpen.spriteId == secondOpen.spriteId) // 두 이미지의 스프라이트 ID 비교
         {
             
-            score++; // 일치하면 점수 증가
-            Scoretxt.text= "Score : " + score.ToString() ;
-            UserScoretxt.text = "0" + score.ToString() ;
-          
-            correct_sfx.Play();
-           
+            score++;                                         // 일치하면 점수 증가
+            Scoretxt.text= "Score : " + score.ToString();    // 실시간 스코어 텍스트로 출력
+            
+
+            correct_sfx.Play();                              // correct_sfx 재생
 
             Vector3 originalScale = new Vector3(1, 1, 1);
             Vector3 targetScale = new Vector3(1.5f, 1.5f, 1.5f);
@@ -225,10 +221,10 @@ public class GameControllerScript : MonoBehaviour
 
             if (score == 10) 
             {
+
+                Main_BGM2.Stop();
                 Score();
                 ResultBG.gameObject.SetActive(true);
-                Main_BGM2.Stop();
-                
                 
 
                 //어떤 컴포넌트에 배정할거임?
@@ -238,9 +234,7 @@ public class GameControllerScript : MonoBehaviour
                 HomeBtn = GameObject.Find("Home").GetComponent<Button>();
                 UserScore = GameObject.Find("UserScoretxt").GetComponent<Text>();  
 
-
             }
-
         }
         else
         {
@@ -265,8 +259,6 @@ public class GameControllerScript : MonoBehaviour
     public List<int> gatchPerList;
     public List<int> rewards; // 줄 애들
 
-
-
     void Setting() // 씬 들어가자마자 한번 하면 됨
     {
         gatchPerList = new List<int>();
@@ -279,13 +271,11 @@ public class GameControllerScript : MonoBehaviour
             if ((int)data_Dialog[i]["Nowrank"] == rank)
             {
                 gatchPerList.Add((int)data_Dialog[i]["Percentage"]);
-                gatchIdList.Add((int)data_Dialog[i]["Percentage"]);
+                gatchIdList.Add((int)data_Dialog[i]["Goods"]);
                 string imageString = "Goods" + data_Dialog[i]["Goods"].ToString();
                 goodsSprites.Add(Resources.Load<Sprite>(imageString));
-
             }
         }
-
     }
 
     public void GetGoods(int _count) // 실제 가챠를 하는 부분. count에는 뽑고 싶은 수량 넣기
@@ -314,23 +304,30 @@ public class GameControllerScript : MonoBehaviour
             RewardsImage[i].sprite = rewardGoods[i];
             RewardsImage[i].gameObject.SetActive(true); // 바뀌었으니 켜라
         }
+        Save();
     }
 
-    void GetItems(int maxValue) // 뽑기. 랜덤 최대값 받기
+    void GetItems(int maxValue)
     {
-        int randValue = UnityEngine.Random.Range(0, maxValue); // 뽑을 애의 해당 가중치량
-        int checkUpper = 0; // 가중치 체크용
+        int randValue = UnityEngine.Random.Range(0, maxValue);
+        Debug.Log("Random Value: " + randValue);
+
+        int checkUpper = 0;
         int checkLower = 0;
+
         for (int i = 0; i < gatchIdList.Count; i++)
         {
             checkUpper += gatchPerList[i];
+            Debug.Log("Check Upper: " + checkUpper);
+
             if (i == 0)
             {
-                checkUpper += gatchPerList[i];
                 if (randValue < checkUpper)
                 {
                     rewards.Add(gatchIdList[i]);
                     rewardGoods.Add(goodsSprites[i]);
+                    Debug.Log("Reward Added: " + gatchIdList[i]);
+                    break;
                 }
             }
             else
@@ -339,33 +336,37 @@ public class GameControllerScript : MonoBehaviour
                 {
                     rewards.Add(gatchIdList[i]);
                     rewardGoods.Add(goodsSprites[i]);
+                    Debug.Log("Reward Added: " + gatchIdList[i]);
+                    break;
                 }
             }
-            checkLower = checkUpper;
 
+            checkLower = checkUpper;
         }
     }
 
 
     public List<Sprite> rewardGoods = new List<Sprite>();
 
-    public int _count = 0;// 몇개 줄 지 설정하는 변수
+   public int _count = 0;// 몇개 줄 지 설정하는 변수
    public void Score() // 이름 바꿔. => 점수에 따라 가챠 수량 설정 하는 부분이라서
     {
-        //Scoretxt.text = score.ToString();
+        UserScoretxt.text = "0" + score.ToString();      // 최종 유저 스코어 텍스트로 출력
 
         PauseGame();
+        stopBg.gameObject.SetActive(false);
+
         //굿즈 지급
         if (score >= 10) // 바꿔
         {
             _count = 3;
 
         }
-        else if (score < 10 && score >= 5)
+        else if (score < 10 && score >= 3)
         {
             _count = 2;
         }
-        else if (score < 5 && score >= 0)
+        else if (score < 3 && score >= 0)
         {
             _count = 1;
         }
@@ -377,12 +378,139 @@ public class GameControllerScript : MonoBehaviour
        
         GetGoods(_count);
 
-        Save();
+       
     }
 
 
     public void Save()
     {
+
+        for (int i = 0; i < rewards.Count; i++)
+        {
+            int rewardId = rewards[i];
+            Debug.Log("리워드 아이디 " + rewardId);
+            // 보상을 ID에 따라 DataManager.Instance에 매핑
+            switch (rewardId)
+            {
+                case 1011:
+                    DataManager.Instance.goods1011++;
+                    break;
+                case 1012:
+                    DataManager.Instance.goods1012++;
+                    break;
+                case 1021:
+                    DataManager.Instance.goods1021++;
+                    break;
+                case 1022:
+                    DataManager.Instance.goods1022++;
+                    break;
+                case 1031:
+                    DataManager.Instance.goods1031++;
+                    break;
+                case 1032:
+                    DataManager.Instance.goods1032++;
+                    break;
+                case 1041:
+                    DataManager.Instance.goods1041++;
+                    break;
+                case 1042:
+                    DataManager.Instance.goods1042++;
+                    break;
+                case 1051:
+                    DataManager.Instance.goods1051++;
+                    break;
+                case 1052:
+                    DataManager.Instance.goods1052++;
+                    break;
+                case 2011:
+                    DataManager.Instance.goods2011++;
+                    break;
+                case 2012:
+                    DataManager.Instance.goods2012++;
+                    break;
+                case 2022:
+                    DataManager.Instance.goods2022++;
+                    break;
+                case 2031:
+                    DataManager.Instance.goods2031++;
+                    break;
+                case 2041:
+                    DataManager.Instance.goods2041++;
+                    break;
+                case 2042:
+                    DataManager.Instance.goods2042++;
+                    break;
+                case 2051:
+                    DataManager.Instance.goods2051++;
+                    break;
+                case 2052:
+                    DataManager.Instance.goods2052++;
+                    break;
+                case 3011:
+                    DataManager.Instance.goods3011++;
+                    break;
+                case 3012:
+                    DataManager.Instance.goods3012++;
+                    break;
+                case 3021:
+                    DataManager.Instance.goods3021++;
+                    break;
+                case 3022:
+                    DataManager.Instance.goods3022++;
+                    break;
+                case 3031:
+                    DataManager.Instance.goods3031++;
+                    break;
+                case 3032:
+                    DataManager.Instance.goods3032++;
+                    break;
+                case 3041:
+                    DataManager.Instance.goods3041++;
+                    break;
+                case 3042:
+                    DataManager.Instance.goods3042++;
+                    break;
+                case 3051:
+                    DataManager.Instance.goods3051++;
+                    break;
+                case 4051:
+                    DataManager.Instance.goods4051++;
+                    break;
+                case 4052:
+                    DataManager.Instance.goods4052++;
+                    break;
+                case 4053:
+                    DataManager.Instance.goods4053++;
+                    break;
+                case 4054:
+                    DataManager.Instance.goods4054++;
+                    break;
+                case 4055:
+                    DataManager.Instance.goods4055++;
+                    break;
+                case 4056:
+                    DataManager.Instance.goods4056++;
+                    break;
+                case 4057:
+                    DataManager.Instance.goods4057++;
+                    break;
+                case 4058:
+                    DataManager.Instance.goods4058++;
+                    break;
+                case 4059:
+                    DataManager.Instance.goods4059++;
+                    break;
+                case 4060:
+                    DataManager.Instance.goods4060++;
+                    break;
+
+                default:
+                    break;
+
+                    // 다른 보상 ID에 대한 케이스 추가...
+            }
+
+        }
         // PlayerPrefs에 현재 값 저장
         PlayerPrefs.SetInt("Goods1011", DataManager.Instance.goods1011);
         PlayerPrefs.SetInt("Goods1012", DataManager.Instance.goods1012);
@@ -390,6 +518,10 @@ public class GameControllerScript : MonoBehaviour
         PlayerPrefs.SetInt("Goods1022", DataManager.Instance.goods1022);
         PlayerPrefs.SetInt("Goods1031", DataManager.Instance.goods1031);
         PlayerPrefs.SetInt("Goods1032", DataManager.Instance.goods1032);
+        PlayerPrefs.SetInt("Goods1041", DataManager.Instance.goods1041);
+        PlayerPrefs.SetInt("Goods1042", DataManager.Instance.goods1042);
+        PlayerPrefs.SetInt("Goods1051", DataManager.Instance.goods1051);
+        PlayerPrefs.SetInt("Goods1052", DataManager.Instance.goods1052);
 
         PlayerPrefs.SetInt("Goods2011", DataManager.Instance.goods2011);
         PlayerPrefs.SetInt("Goods2012", DataManager.Instance.goods2012);
@@ -397,6 +529,10 @@ public class GameControllerScript : MonoBehaviour
         PlayerPrefs.SetInt("Goods2022", DataManager.Instance.goods2022);
         PlayerPrefs.SetInt("Goods2031", DataManager.Instance.goods2031);
         PlayerPrefs.SetInt("Goods2032", DataManager.Instance.goods2032);
+        PlayerPrefs.SetInt("Goods2041", DataManager.Instance.goods2041);
+        PlayerPrefs.SetInt("Goods2042", DataManager.Instance.goods2042);
+        PlayerPrefs.SetInt("Goods2051", DataManager.Instance.goods2051);
+        PlayerPrefs.SetInt("Goods2052", DataManager.Instance.goods2052);
 
         PlayerPrefs.SetInt("Goods3011", DataManager.Instance.goods3011);
         PlayerPrefs.SetInt("Goods3012", DataManager.Instance.goods3012);
@@ -404,7 +540,68 @@ public class GameControllerScript : MonoBehaviour
         PlayerPrefs.SetInt("Goods3022", DataManager.Instance.goods3022);
         PlayerPrefs.SetInt("Goods3031", DataManager.Instance.goods3031);
         PlayerPrefs.SetInt("Goods3032", DataManager.Instance.goods3032);
+        PlayerPrefs.SetInt("Goods3041", DataManager.Instance.goods3041);
+        PlayerPrefs.SetInt("Goods3042", DataManager.Instance.goods3042);
+        PlayerPrefs.SetInt("Goods3051", DataManager.Instance.goods3051);
+        PlayerPrefs.SetInt("Goods3052", DataManager.Instance.goods3052);
+
+        PlayerPrefs.SetInt("Goods4051", DataManager.Instance.goods4051);
+        PlayerPrefs.SetInt("Goods4052", DataManager.Instance.goods4052);
+        PlayerPrefs.SetInt("Goods4053", DataManager.Instance.goods4053);
+        PlayerPrefs.SetInt("Goods4054", DataManager.Instance.goods4054);
+        PlayerPrefs.SetInt("Goods4055", DataManager.Instance.goods4055);
+        PlayerPrefs.SetInt("Goods4056", DataManager.Instance.goods4056);
+        PlayerPrefs.SetInt("Goods4057", DataManager.Instance.goods4057);
+        PlayerPrefs.SetInt("Goods4058", DataManager.Instance.goods4058);
+        PlayerPrefs.SetInt("Goods4059", DataManager.Instance.goods4059);
+        PlayerPrefs.SetInt("Goods4060", DataManager.Instance.goods4060);
+
         PlayerPrefs.Save();
+
+        Debug.Log(DataManager.Instance.goods1011);
+        Debug.Log(DataManager.Instance.goods1012);
+        Debug.Log(DataManager.Instance.goods2011);
+        Debug.Log(DataManager.Instance.goods2012);
+        Debug.Log(DataManager.Instance.goods3011);
+        Debug.Log(DataManager.Instance.goods3012);
+        Debug.Log(DataManager.Instance.goods1021);
+        Debug.Log(DataManager.Instance.goods1022);
+        Debug.Log(DataManager.Instance.goods2021);
+        Debug.Log(DataManager.Instance.goods2022);
+        Debug.Log(DataManager.Instance.goods3021);
+        Debug.Log(DataManager.Instance.goods3022);
+        Debug.Log(DataManager.Instance.goods1031);
+        Debug.Log(DataManager.Instance.goods1032);
+        Debug.Log(DataManager.Instance.goods2031);
+        Debug.Log(DataManager.Instance.goods2032);
+        Debug.Log(DataManager.Instance.goods3031);
+        Debug.Log(DataManager.Instance.goods3032);
+        Debug.Log(DataManager.Instance.goods1041);
+        Debug.Log(DataManager.Instance.goods1042);
+        Debug.Log(DataManager.Instance.goods2041);
+        Debug.Log(DataManager.Instance.goods2042);
+        Debug.Log(DataManager.Instance.goods3041);
+        Debug.Log(DataManager.Instance.goods3042);
+        Debug.Log(DataManager.Instance.goods1051);
+        Debug.Log(DataManager.Instance.goods1052);
+        Debug.Log(DataManager.Instance.goods2051);
+        Debug.Log(DataManager.Instance.goods2052);
+        Debug.Log(DataManager.Instance.goods3051);
+        Debug.Log(DataManager.Instance.goods3052);
+        Debug.Log(DataManager.Instance.goods4051);
+        Debug.Log(DataManager.Instance.goods4052);
+        Debug.Log(DataManager.Instance.goods4053);
+        Debug.Log(DataManager.Instance.goods4054);
+        Debug.Log(DataManager.Instance.goods4055);
+        Debug.Log(DataManager.Instance.goods4056);
+        Debug.Log(DataManager.Instance.goods4057);
+        Debug.Log(DataManager.Instance.goods4058);
+        Debug.Log(DataManager.Instance.goods4059);
+        Debug.Log(DataManager.Instance.goods4060);
+
+
+
+
 
     }
     // 게임 재시작 함수
