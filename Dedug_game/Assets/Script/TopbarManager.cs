53db782f452489e5bbd2500e8609e1ef0007b9c7
teolkongge <staysilver@naver.com>
2024-01-19 09:16:47 +0900

    0119 테스트 버튼 삭제 및 탑바에 클리어 버튼 부여

--- Dedug_game/Assets/Script/TopbarManager.cs
+++ Dedug_game/Assets/Script/TopbarManager.cs
@@ -58,11 +58,7 @@ public class TopbarManager : MonoBehaviour
 
     }
 
-    public void Save()
-    {
-        PlayerPrefs.SetInt("NowGold", DataManager.Instance.nowGold);
-        PlayerPrefs.Save();
-    }
+  
 
     private void Awake()
     {
@@ -197,9 +193,121 @@ public class TopbarManager : MonoBehaviour
         SceneManager.LoadScene("HomeScene");
     }
     
-    
-  
-  
+    public void Clear()
+    {
+        DataManager.Instance.nowRank = 0;
+        DataManager.Instance.nowGold = 0;
+        DataManager.Instance.feverNum = 0;
+        DataManager.Instance.goods1011 = 0;
+        DataManager.Instance.goods2011 = 0;
+        DataManager.Instance.goods3011 = 0;
+        DataManager.Instance.goods1012 = 0;
+        DataManager.Instance.goods2012 = 0;
+        DataManager.Instance.goods3012 = 0;
+        DataManager.Instance.goods1021 = 0;
+        DataManager.Instance.goods2021 = 0;
+        DataManager.Instance.goods3021 = 0;
+        DataManager.Instance.goods1022 = 0;
+        DataManager.Instance.goods2022 = 0;
+        DataManager.Instance.goods3022 = 0;
+        DataManager.Instance.goods1031 = 0;
+        DataManager.Instance.goods2031 = 0;
+        DataManager.Instance.goods3031 = 0;
+        DataManager.Instance.goods1032 = 0;
+        DataManager.Instance.goods2032 = 0;
+        DataManager.Instance.goods3032 = 0;
+        DataManager.Instance.goods1041 = 0;
+        DataManager.Instance.goods2041 = 0;
+        DataManager.Instance.goods3041 = 0;
+        DataManager.Instance.goods1042 = 0;
+        DataManager.Instance.goods2042 = 0;
+        DataManager.Instance.goods3042 = 0;
+        DataManager.Instance.goods1041 = 0;
+        DataManager.Instance.goods2041 = 0;
+        DataManager.Instance.goods3041 = 0;
+        DataManager.Instance.goods1051 = 0;
+        DataManager.Instance.goods2051 = 0;
+        DataManager.Instance.goods3051 = 0;
+        DataManager.Instance.goods1052 = 0;
+        DataManager.Instance.goods2052 = 0;
+        DataManager.Instance.goods3052 = 0;
+        DataManager.Instance.goods4051 = 0;
+        DataManager.Instance.goods4052 = 0;
+        DataManager.Instance.goods4053 = 0;
+        DataManager.Instance.goods4054 = 0;
+        DataManager.Instance.goods4055 = 0;
+        DataManager.Instance.goods4056 = 0;
+        DataManager.Instance.goods4057 = 0;
+        DataManager.Instance.goods4058 = 0;
+        DataManager.Instance.goods4059 = 0;
+        DataManager.Instance.goods4060 = 0;
+        DataManager.Instance.story1_1 = 0;
+        DataManager.Instance.story1_2 = 0;
+        DataManager.Instance.story1_3 = 0;
+        DataManager.Instance.story2_1 = 0;
+        DataManager.Instance.story2_2 = 0;
+        DataManager.Instance.story2_3 = 0;
+        DataManager.Instance.story3_1 = 0;
+        DataManager.Instance.story3_2 = 0;
+        DataManager.Instance.story3_3 = 0;
+        DataManager.Instance.story4_1 = 0;
+        DataManager.Instance.story4_2 = 0;
+        DataManager.Instance.story4_3 = 0;
+        DataManager.Instance.storyID = 0;
+        Save();
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("NowGold", DataManager.Instance.nowGold);
+        PlayerPrefs.SetInt("NowRank", DataManager.Instance.nowRank);
+        PlayerPrefs.SetInt("FeverNum", DataManager.Instance.feverNum);
+        PlayerPrefs.SetInt("StoryID", DataManager.Instance.storyID);
+        PlayerPrefs.SetInt("Story1_1", DataManager.Instance.story1_1);
+        PlayerPrefs.SetInt("Story1_2", DataManager.Instance.story1_2);
+        PlayerPrefs.SetInt("Story1_3", DataManager.Instance.story1_2);
+        PlayerPrefs.SetInt("Story2_1", DataManager.Instance.story2_1);
+        PlayerPrefs.SetInt("Story2_2", DataManager.Instance.story2_2);
+        PlayerPrefs.SetInt("Story2_3", DataManager.Instance.story2_2);
+        PlayerPrefs.SetInt("Story3_1", DataManager.Instance.story3_1);
+        PlayerPrefs.SetInt("Story3_2", DataManager.Instance.story3_2);
+        PlayerPrefs.SetInt("Story3_3", DataManager.Instance.story3_3);
+        PlayerPrefs.SetInt("Story4_1", DataManager.Instance.story4_1);
+        PlayerPrefs.SetInt("Story4_2", DataManager.Instance.story4_2);
+        PlayerPrefs.SetInt("Story4_3", DataManager.Instance.story4_3);
+        PlayerPrefs.SetInt("Goods1011", DataManager.Instance.goods1011);
+        PlayerPrefs.SetInt("Goods1012", DataManager.Instance.goods1012);
+        PlayerPrefs.SetInt("Goods1021", DataManager.Instance.goods1021);
+        PlayerPrefs.SetInt("Goods1022", DataManager.Instance.goods1022);
+        PlayerPrefs.SetInt("Goods1031", DataManager.Instance.goods1031);
+        PlayerPrefs.SetInt("Goods1032", DataManager.Instance.goods1032);
+        PlayerPrefs.SetInt("Goods1041", DataManager.Instance.goods1041);
+        PlayerPrefs.SetInt("Goods1042", DataManager.Instance.goods1042);
+        PlayerPrefs.SetInt("Goods1051", DataManager.Instance.goods1051);
+        PlayerPrefs.SetInt("Goods1052", DataManager.Instance.goods1052);
+        PlayerPrefs.SetInt("Goods2011", DataManager.Instance.goods2011);
+        PlayerPrefs.SetInt("Goods2012", DataManager.Instance.goods2012);
+        PlayerPrefs.SetInt("Goods2021", DataManager.Instance.goods2021);
+        PlayerPrefs.SetInt("Goods2022", DataManager.Instance.goods2022);
+        PlayerPrefs.SetInt("Goods2031", DataManager.Instance.goods2031);
+        PlayerPrefs.SetInt("Goods2032", DataManager.Instance.goods2032);
+        PlayerPrefs.SetInt("Goods2041", DataManager.Instance.goods2041);
+        PlayerPrefs.SetInt("Goods2042", DataManager.Instance.goods2042);
+        PlayerPrefs.SetInt("Goods2051", DataManager.Instance.goods2051);
+        PlayerPrefs.SetInt("Goods2052", DataManager.Instance.goods2052);
+        PlayerPrefs.SetInt("Goods3011", DataManager.Instance.goods3011);
+        PlayerPrefs.SetInt("Goods3012", DataManager.Instance.goods3012);
+        PlayerPrefs.SetInt("Goods3021", DataManager.Instance.goods3021);
+        PlayerPrefs.SetInt("Goods3022", DataManager.Instance.goods3022);
+        PlayerPrefs.SetInt("Goods3031", DataManager.Instance.goods3031);
+        PlayerPrefs.SetInt("Goods3032", DataManager.Instance.goods3032);
+        PlayerPrefs.SetInt("Goods3041", DataManager.Instance.goods3041);
+        PlayerPrefs.SetInt("Goods3042", DataManager.Instance.goods3042);
+        PlayerPrefs.SetInt("Goods3051", DataManager.Instance.goods3051);
+        PlayerPrefs.SetInt("Goods3052", DataManager.Instance.goods3052);
+        PlayerPrefs.Save();
+    }
+
 
 }
 
